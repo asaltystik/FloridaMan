37ca6981fe448af2117ca8c7df7b443643d659c2
goteem <blueapple1337@gmail.com>
2020-03-11 12:01:56 -0400

    Player Controller

--- sauce/Actors/Actor.gd
+++ sauce/Actors/Actor.gd
@@ -1,2 +1,6 @@
 extends KinematicBody2D
 class_name  Actor
+
+const ON_FLOOR:= Vector2.UP
+var _velocity:= Vector2.ZERO
+var gravity:= 3000.0
--- sauce/Actors/Player.gd
+++ sauce/Actors/Player.gd
@@ -1,1 +1,15 @@
 extends Actor
+
+func _physics_process(delta):
+	var direction:= get_direction()
+	var speed:= 10.0
+	_velocity.x += speed*direction.x
+	_velocity = move_and_slide(_velocity, ON_FLOOR)
+	
+
+
+func get_direction() -> Vector2:
+	return Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		1.0
+	)
