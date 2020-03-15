363f1cb0e26246720de87bd697adee99fb30d63e
Wouldnt you like to know weatherboy <spamcout27@gmail.com>
2020-03-15 18:09:42 -0400

    Enemy Scene

---
+++ sauce/Actors/Enemy.gd
@@ -0,0 +1,11 @@
+extends "res://sauce/Actors/Actor.gd"
+
+
+func _ready():
+	set_physics_process(false)
+
+func _physics_process(delta):
+	_velocity.y += gravity * delta
+	if is_on_wall():
+		_velocity.x *= -1.0
+	_velocity = move_and_slide(_velocity, ON_FLOOR)
